a42c9e64d35d9f32c76731471b6e24ed935765a2
xinw0911 <cindywanghugetooth@gmail.com>
2024-05-12 21:57:45 -0700

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  reporters: [
+    'default',
+    [ 'jest-junit', {
+      outputDirectory: ./,
+      outputName: testreport,
+    } ]
+  ],
+  collectCoverage: true,
+  coverageDirectory: './',
+  coverageReporters: ['cobertura'],
+};
